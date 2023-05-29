16308eb8c2a4d4d2ede54a485f61dc8516a03319
AlexMao02 <125329557+AlexMao02@users.noreply.github.com>
2023-05-29 20:27:59 +0800

    Update work.py

--- work.py
+++ work.py
@@ -16,12 +16,12 @@ import datetime as dt
 import streamlit as st
 
 # Observe recent changes
-start = dt.datetime(2019, 1, 1).strftime('%Y-%m-%d')
+start = dt.datetime(2015, 1, 1).strftime('%Y-%m-%d')
 end =  dt.date.today()
-#end='2023-2-23'
+#end='2023-5-1'
 
 # set ticker's symbol in yahoo stock
-ticker='2330.TW'
+ticker='2618.TW'
 
 # Downloading data
 
@@ -35,8 +35,8 @@ df['bb_lower'] = pandas_ta.bbands(close=df['Adj Close'],length=20).iloc[:,0]
 df['bb_upper'] = pandas_ta.bbands(close=df['Adj Close'],length=20).iloc[:,2]
 df['forward_1d'] = df['Adj Close'].pct_change(1).shift(-1)
 
-fig=ex.line(df, x=df.index, y=['Adj Close','bb_lower','bb_upper'], title='台鋼  Adj Close with Bollinger Bands')
-fig.update_layout(title_text=f'台鋼 Adj Close with Bollinger Bands', title_x=0.5);
+fig=ex.line(df, x=df.index, y=['Adj Close','bb_lower','bb_upper'], title='長榮航 (2618.TW) Adj Close with Bollinger Bands')
+fig.update_layout(title_text=f'長榮航 (2618.TW) Adj Close with Bollinger Bands', title_x=0.5);
 
 # Plot!
 st.plotly_chart(fig, use_container_width=True)
